7f3da02b586b0c08284167ea3dd26c5bc72ab78a
Alla <allasamoilenko@gmx.de>
2024-11-02 18:41:35 +0200

    task2

--- main.py
+++ main.py
@@ -16,3 +16,28 @@ class Fraction:
 fraction1 = Fraction(1, 2)
 fraction2 = Fraction(3, 4)
 print("Total number of Fraction objects created:", Fraction.count_fraction())
+
+#task2
+class TemperatureConverter:
+    count = 0
+    @staticmethod
+    def celsius_to_fahrenheit(celsius):
+        return (celsius * 9/5) + 32
+
+    @staticmethod
+    def fahrenheit_to_celsius(fahrenheit):
+        return (fahrenheit - 32) * 5/9
+
+    @staticmethod
+    def count_conversion():
+        TemperatureConverter.count += 1
+        return TemperatureConverter.count
+celsius = 25
+fahrenheit = TemperatureConverter.celsius_to_fahrenheit(celsius)
+print(f"{celsius}°C is {fahrenheit}°F")
+print("Total temperature conversions:", TemperatureConverter.count_conversion())
+
+fahrenheit = 77
+celsius = TemperatureConverter.fahrenheit_to_celsius(fahrenheit)
+print(f"{fahrenheit}°F is {celsius}°C")
+print("Total temperature conversions:", TemperatureConverter.count_conversion())
